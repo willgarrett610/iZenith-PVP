5713ad74b4d2050de6f392a5e26f30d2a793c6b8
Will Garrett <willgarrett610@gmail.com>
2016-08-14 20:39:30 -0600

    Add permission for /buy

--- src/net/izenith/Commands/Buy.java
+++ src/net/izenith/Commands/Buy.java
@@ -32,12 +32,12 @@ public class Buy implements HubCommand{
 
 	@Override
 	public boolean hasPermission() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public Permission getPermission() {
-		return null;
+		return new Permission("shop.buy");
 	}
 
 }
